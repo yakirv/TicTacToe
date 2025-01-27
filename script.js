c5af60c3ed239b067feaa9b0056a3ed5d403a700
Yakirv <yakirv@gmail.com>
2025-01-27 13:52:26 +0200

    players logic added

--- script.js
+++ script.js
@@ -21,38 +21,65 @@ let gameBoard = [
     })() 
 
  */
-function marker()
+
+
+
+(function marker()
 {   
     const gameBoardContainer = document.getElementById('game-board');
     const boardChildren = gameBoardContainer.children;
-
-
+    let turnStatus = true;
+    
     for (let i = 0; i < boardChildren.length; i++) {
         const element = boardChildren[i];
         const rawIndex = element.getAttribute('rawIndex');
         const columnIndex = element.getAttribute('columnIndex');
         
-        
         element.addEventListener('click', ()=>{
-        
             if (element.textContent == '')
-            {element.textContent= 'X'; 
-             gameBoard[rawIndex][columnIndex] = 'x'
-             
-             if (checkWin())
-                {
-                const message = document.getElementById('message');
-                message.textContent = 'YOU WIN';
+              {
+                if (turnStatus)
+                     {
+                    element.textContent= 'X'; 
+                    gameBoard[rawIndex][columnIndex] =  'X';
+                    if (checkWin())
+                         {  
+                            const message = document.getElementById('message');
+                            message.textContent = 'PLAYER ONE WIN';
+                         }
+                         const isTie= checktie(boardChildren);
+                         if(isTie)
+                        {
+                        message.textContent = 'Its a TIE'; 
+                         }
+                      turnStatus=false;
+                    }
+                    if ( !turnStatus && element.textContent != 'X' )
+                        {
 
+                       element.textContent = 'O' ; 
+                       gameBoard[rawIndex][columnIndex] =  'O';
+                       playerTwoTurn = false;
+                       if (checkWin())
+                            {  
+                               const message = document.getElementById('message');
+                               message.textContent = 'PLAYER Two WIN';
+                            }
+                            const isTie= checktie(boardChildren);
+                            if(isTie)
+                           {
+                           message.textContent = 'Its a TIE'; 
+                            }    
+                        turnStatus =true;
+                       }
                 
-                }
-            }
-    })
+               }
             
-      }
-
-}
-marker();
+            })        
+    }
+         
+ })();
+  
 
 
 
@@ -77,6 +104,21 @@ function checkWin()
     return true;
    if (gameBoard[0][2] == gameBoard[1][1] & gameBoard[1][1]==gameBoard[2][0] &  gameBoard[1][1]!= '')
     return true;
+   
+}
+
+function checktie(cells)
+{
+    for (let i = 0; i < cells.length; i++) {
+        const element = cells[i];
+        const textContent = element.textContent;
+    
+        if (textContent === "")
+            { 
+          return false;ately
+              }
+      }
+      return true; 
 }
 
 
