35a508aa3c4c851cdaa15b31875db8bbaa80f9ff
Yakirv <yakirv@gmail.com>
2025-01-27 11:46:44 +0200

    building the game

---
+++ script.js
@@ -0,0 +1,82 @@
+let gameBoard = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+];
+
+
+/* (function createBoard()
+    {
+        const gameBoardContainer = document.getElementById('game-board');
+        for (let i =1; i < 10; i++)
+        {
+            const cell = document.createElement('div');
+            cell.classList = `cell-${i}`;
+            cell.style.backgroundColor ='white';
+            gameBoardContainer.appendChild(cell);
+            console.log ("the FIRSST class is", cell);
+           
+        }
+      
+    })() 
+
+ */
+function marker()
+{   
+    const gameBoardContainer = document.getElementById('game-board');
+    const boardChildren = gameBoardContainer.children;
+
+
+    for (let i = 0; i < boardChildren.length; i++) {
+        const element = boardChildren[i];
+        const rawIndex = element.getAttribute('rawIndex');
+        const columnIndex = element.getAttribute('columnIndex');
+        
+        
+        element.addEventListener('click', ()=>{
+        
+            if (element.textContent == '')
+            {element.textContent= 'X'; 
+             gameBoard[rawIndex][columnIndex] = 'x'
+             
+             if (checkWin())
+                {
+                const message = document.getElementById('message');
+                message.textContent = 'YOU WIN';
+
+                
+                }
+            }
+    })
+            
+      }
+
+}
+marker();
+
+
+
+function checkWin()
+{
+    //rows
+  if (gameBoard[0][0] == gameBoard[0][1] & gameBoard[0][1]==gameBoard[0][2] &  gameBoard[0][1]!= '')
+    return true;
+  if (gameBoard[1][0] == gameBoard[1][1] & gameBoard[1][1]==gameBoard[1][2]&  gameBoard[1][1]!= '')
+      return true;  
+  if (gameBoard[2][0] == gameBoard[2][1] & gameBoard[2][1]==gameBoard[2][2]&  gameBoard[2][1]!= '')
+        return true;
+   //columns
+  if (gameBoard[0][0] == gameBoard[1][0] & gameBoard[1][0]==gameBoard[2][0] &  gameBoard[1][0]!= '')
+    return true;
+  if (gameBoard[0][1] == gameBoard[1][1] & gameBoard[1][1]==gameBoard[2][1] &  gameBoard[1][1]!= '')
+      return true;  
+  if (gameBoard[0][2] == gameBoard[1][2] & gameBoard[1][2]==gameBoard[2][2] &  gameBoard[1][2]!= '')
+        return true;
+   //diagonals
+   if (gameBoard[0][0] == gameBoard[1][1] & gameBoard[1][1]==gameBoard[2][2] &  gameBoard[1][1]!= '')
+    return true;
+   if (gameBoard[0][2] == gameBoard[1][1] & gameBoard[1][1]==gameBoard[2][0] &  gameBoard[1][1]!= '')
+    return true;
+}
+
+
